833d099da5a196a3a9c914462b7c4deda2255e25
Prem Kumar <premkumar@metapointer.com>
2023-07-25 12:27:03 +0530

    changes for cart total

--- src/app/services/catalogue-service.service.ts
+++ src/app/services/catalogue-service.service.ts
@@ -10,6 +10,7 @@ export class CatalogueServiceService {
   constructor(private http: HttpClient) { }
   api : string = 'https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json';
   cartItems:any = [];
+  cartTotal:number=0;
   products:any;
   filteredData:any;
   
@@ -21,56 +22,100 @@ export class CatalogueServiceService {
     );
   };
 
+  calculateCartTotal(){
+    this.cartTotal=0
+    this.cartItems.map((item:any)=>{
+      this.cartTotal+=item.price*item.quantity;
+    });
+  };
+
   addItemsCart(id: number) {
-    const cartItem = this.products.find((p: any) => p.id === id && p.quantity > 0);
-    if (cartItem) {
-      const existingItem = this.cartItems.find((item: any) => item.id === id);
-      if (existingItem && existingItem.quantity < cartItem.quantity) {
-        existingItem.quantity++;
-      } 
-      else if (!existingItem) {
-        const newItem = { ...cartItem, quantity: 1 };
-        this.cartItems.push(newItem);
-      } 
-      else {
-        console.error('Item quantity limit exceeded');
+    try {
+      const cartItem = this.products.find((p: any) => p.id === id && p.quantity > 0);
+      if (cartItem) {
+        const existingItem = this.cartItems.find((item: any) => item.id === id);
+        if (existingItem && existingItem.quantity < cartItem.quantity) {
+          existingItem.quantity++;
+        } 
+        else if (!existingItem) {
+          const newItem = { ...cartItem, quantity: 1 };
+          this.cartItems.push(newItem);
+        } 
+        else {
+          console.error('Item quantity limit exceeded');
+        }
       }
-    } 
-    else {
+      else {
+        console.error('Item is not available!');
+      }
+    }
+    catch(err) {
       console.error('Item is not available!');
+    }
+    finally {
+      this.calculateCartTotal();
     }
   };
 
   removeFromCart(id:number){
-    this.cartItems = this.cartItems.filter((item:any)=>{
-      if (item.id!==id) {
-        return true;
-      }
-      return false;
-    });
+    try{
+      this.cartItems = this.cartItems.filter((item:any)=>{
+        if (item.id!==id) {
+          return true;
+        }
+        return false;
+      });
+    }
+    catch(err){
+      console.error(err);
+    }
+    finally {
+      this.calculateCartTotal();
+    }
   };
 
   removeOneItem(id:number){
-    this.cartItems = this.cartItems.map((item:any)=>{
-      if (item.id==id && item.quantity>0) {
-          item.quantity--; 
-        }else{
-          console.error('error removing item!');
+    try{
+      this.cartItems = this.cartItems.map((item:any)=>{
+        if (item.id==id){ 
+          if(item.quantity>0) {
+            item.quantity--; 
+          }else{
+            throw new Error('error removing item!');
+          }
         }
-      return item;
-    });
+        return item;
+      });
+    }
+    catch(err){
+      console.error(err);
+    }
+    finally {
+      this.calculateCartTotal();
+    }
   };
 
   addOneItem(id:number){
-    const cartItem = this.products.find((p: any) => p.id === id);
-    this.cartItems = this.cartItems.map((item:any)=>{
-      if (item.id==id && item.quantity<cartItem.quantity) {
-          item.quantity++; 
-        }else{
-          console.error('error');
+    try{
+      const cartItem = this.products.find((p: any) => p.id === id);
+      this.cartItems = this.cartItems.map((item:any)=>{
+        if (item.id==id) {
+          if(item.quantity<cartItem.quantity){
+            item.quantity++;
+          }
+          else{
+            throw new Error('error! quantity exceeded!!');
+          }
         }
-      return item;
-    });
+        return item;
+      });
+    }
+    catch(err){
+      console.error(err);
+    }
+    finally {
+      this.calculateCartTotal();
+    }
   };
 
 }
